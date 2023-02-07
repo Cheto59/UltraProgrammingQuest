4178def14f5e4db37c626f21402ca5c81dc4afd7
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-02-07 15:20:38 -0500

    basic interpreter setup

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
-"use strict";
-console.log("Hello World!");
+import { jsPython } from "/node_modules/jspython-interpreter/dist/jspython-interpreter.esm.js";
+
+function onRun(){
+    var codeEditor = document.getElementById("codeEditor")
+    var script = codeEditor.value
+    const interpreter = jsPython()
+    interpreter.evaluate(script)
+}
+
+var runButton = document.getElementById("runButton")
+runButton.addEventListener('click', onRun)
