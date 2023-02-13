e22392a1e9bc76a1eb64f4c9abe619d1f84e309b
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-02-13 12:16:49 -0500

    css Styles and UI implementation

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,36 @@
-import { jsPython } from "/node_modules/jspython-interpreter/dist/jspython-interpreter.esm.js";
+import { jsPython } from "../node_modules/jspython-interpreter/dist/jspython-interpreter.esm.js";
 
 class Problem {
 
-    constructor(name,description){
+    constructor(name,description,testCasesFile){
         this.name = name
         this.description = description
+        this.testCases = this.setTestCases(testCasesFile)
     }
 
+    //Aquí va la logica de lectura del archivo de texto plano
+    //para convertir en testCases 
+    setTestCases(testCasesFile){
+        const fileReader = new FileReader()
+        var testCases = []
+        fileReader.addEventListener("load", (event) => {
+            const text = event.target.result
+            var testCasesTemp = text.split(" ")
+            for(let i = 0; i<testCasesTemp.length;i+=2){
+                let set = new Set()
+                set.add(testCasesTemp[i])
+                set.add(testCasesTemp[i+1]) 
+                testCases.push(set)
+            }
+        })
+        fileReader.readAsText(testCasesFile)
+        return testCases
+    }
 }
 
 function loadProblem(problem){
-    console.log(problem)
     var option = document.createElement('option')
-    option.value = problem
+    option.value = problem.description
     option.innerHTML = problem.name
     document.getElementById("problemSelect").appendChild(option)
 }
@@ -45,15 +63,20 @@ function onRun(){
 
 function onAddProblem(){
     const name = document.getElementById("nameInput").value
-    if(problems.find(problem => problem.name == name) == null){
-        const description = document.getElementById("descriptionTextArea").value
-        const problem = new Problem(name,description)
-        problems.push(problem)
-        // localStorage.setItem("problems",problems)
-        loadProblem(problem)    
-    }else alert("Ya existe un problema con el mismo nombre")
+    const testCasesFile = document.getElementById("testCasesSelector").files[0]
+    if( testCasesFile != null && testCasesFile.type == "text/plain"){
+        if(problems.find(problem => problem.name == name) == null){
+            const description = document.getElementById("descriptionTextArea").value
+            const problem = new Problem(name,description,testCasesFile)
+            console.log(problem)
+            problems.push(problem)
+            // localStorage.setItem("problems",problems)
+            loadProblem(problem)
+            alert("El problema fue credo correctamente")    
+        }else alert("Ya existe un problema con el mismo nombre")
+    }else alert("No has seleccionado un archivo o el tipo de archivo no es el indicado para los casos de prueba")     
 }
 
-
 document.getElementById("runButton").addEventListener('click', onRun)
-document.getElementById("addProblemButton").addEventListener('click',onAddProblem) 
+document.getElementById("addProblemButton").addEventListener('click',onAddProblem)
+document.getElementById("problemSelect").addEventListener('change', (event)=>{document.getElementById("problemTextArea").value = event.target.value})
